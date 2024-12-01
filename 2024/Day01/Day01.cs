1a0b6d3b077016817deb58f9d4b26ed738ec4140
Elian Caby <54775334+ElianC@users.noreply.github.com>
2024-12-01 16:25:25 +0100

    simplify code

--- 2024/Day01/Day01.cs
+++ 2024/Day01/Day01.cs
@@ -9,63 +9,33 @@ public class Day01 : BaseSolve
     public override void Solve()
     {
         var input = GetInput();
-        var lines = input.Split(Environment.NewLine);
 
-        var leftNumbers = new uint[lines.Length];
-        var rightNumbers = new uint[lines.Length];
+        var numbers = input
+            .Split(Environment.NewLine)
+            .Select(ln => ln.Split("   "))
+            .Select(col => (LeftNumber: uint.Parse(col[0]), RightNumber: uint.Parse(col[1])))
+            .ToList();
 
-        for (var i = 0; i < lines.Length; i++)
-        {
-            var columns = lines[i].Split("   ");
+        var leftNumbers = numbers
+            .Select(e => e.LeftNumber)
+            .Order()
+            .ToList();
+        var rightNumbers = numbers
+            .Select(e => e.RightNumber)
+            .Order()
+            .ToList();
 
-            leftNumbers[i] = uint.Parse(columns[0]);
-            rightNumbers[i] = uint.Parse(columns[1]);
-        }
-
-        var solution1 = CalculateTotalDistanceBetweenSortedPairs(rightNumbers, leftNumbers);
+        var solution1 = leftNumbers
+            .Zip(rightNumbers, (x, y) => Math.Abs(x - (long)y))
+            .Sum();
         Console.WriteLine($"Total distance between the lists: {solution1}");
 
-        var solution2 = CalculateSimilarityScoreBasedOnOccurrences(rightNumbers, leftNumbers);
+        var weights = rightNumbers
+            .CountBy(x => x)
+            .ToDictionary();
+        var solution2 = leftNumbers
+            .Select(num => weights.GetValueOrDefault(num) * num)
+            .Sum();
         Console.WriteLine($"The similarity score between the lists: {solution2}");
-    }
-
-    private static uint CalculateTotalDistanceBetweenSortedPairs(uint[] rightNumbers, uint[] leftNumbers)
-    {
-        uint distance = 0;
-
-        var orderedLeftNumbers = leftNumbers
-            .OrderBy(x => x)
-            .ToArray();
-        var orderedRightNumbers = rightNumbers
-            .OrderBy(x => x)
-            .ToArray();
-
-        for (var i = 0; i < orderedLeftNumbers.Length; i++)
-        {
-            var leftNumber = orderedLeftNumbers[i];
-            var rightNumber = orderedRightNumbers[i];
-
-            if (leftNumber > rightNumber)
-                distance += leftNumber - rightNumber;
-            else
-                distance += rightNumber - leftNumber;
-        }
-
-        return distance;
-    }
-
-    private static uint CalculateSimilarityScoreBasedOnOccurrences(uint[] rightNumbers, uint[] leftNumbers)
-    {
-        uint solution = 0;
-        var uniqueLeftNumbers = leftNumbers.Distinct().ToArray();
-
-        foreach (var number in uniqueLeftNumbers)
-        {
-            var count = rightNumbers.Select(x => x).Count(x => x == number);
-
-            if (count is not 0) solution += (uint)(number * count);
-        }
-
-        return solution;
     }
 }
