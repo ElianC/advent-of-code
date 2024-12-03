894a540af281ba45413db7dad8b1d758e246c7fc
Elian Caby <54775334+ElianC@users.noreply.github.com>
2024-12-03 19:39:09 +0100

    prepare Day04

---
+++ 2024/Day04/Solution.cs
@@ -0,0 +1,13 @@
+namespace AdventOfCode._2024.Day04;
+
+public class Solution : BaseSolve
+{
+    public Solution(string day, string year) : base(day, year)
+    {
+    }
+
+    public override void Solve()
+    {
+        Console.WriteLine("2024 - Day 4");
+    }
+}
