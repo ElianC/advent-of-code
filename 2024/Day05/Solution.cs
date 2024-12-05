d5357e881e70d883c0c25264025092cc0769bc22
Elian Caby <caby.elian01@gmail.com>
2024-12-05 17:39:55 +0100

    Update Solution.cs

--- 2024/Day05/Solution.cs
+++ 2024/Day05/Solution.cs
@@ -6,60 +6,47 @@ public class Solution : BaseSolution
     {
     }
 
-    private static bool CheckIfArrayIsInSpecificOrder(int[] input, int mustBeBefore, int mustBeAfter)
+    public bool IsOrdered(List<int> updatePages, List<List<int>> orderingRules)
     {
-        if (!input.Contains(mustBeBefore) || !input.Contains(mustBeAfter)) return true;
-        
-        var indexMustBeBefore = Array.IndexOf(input, mustBeBefore);
-        var indexMustBeAfter = Array.IndexOf(input, mustBeAfter);
+        var isOrdered = orderingRules
+            .Where(rule => updatePages.Contains(rule[0]) && updatePages.Contains(rule[1]))
+            .All(rule => updatePages.IndexOf(rule[0]) < updatePages.IndexOf(rule[1]));
+                
+        return isOrdered;
 
-        return indexMustBeBefore < indexMustBeAfter;
     }
     
     public override void Solve()
     {
-        var solution1 = 0;
-        var solution2 = 0;
-        
         var inputs = GetInput().Split(Environment.NewLine + Environment.NewLine);
+        
         var pageOrderingRules = inputs[0].Split(Environment.NewLine);
         var pageNumberUpdates = inputs[1].Split(Environment.NewLine);
 
         var orderingRules = pageOrderingRules
-            .Select(e => e.Split("|").Select(int.Parse).ToArray())
-            .ToArray();
+            .Select(e => e.Split("|").Select(int.Parse).ToList())
+            .ToList();
         
         var updatesPages = pageNumberUpdates
-            .Select(e => e.Split(",").Select(int.Parse).ToArray())
-            .ToArray();
+            .Select(e => e.Split(",").Select(int.Parse).ToList())
+            .ToList();
 
-        foreach (var updatePages in updatesPages)
-        {
-            
-            List<bool> arrayCorrectOrder = new();
-            var arrayToSort = updatePages.ToList();
-            
-            foreach (var orderingRule in orderingRules.Reverse())
-            {
-                var isInCorrectOrder = CheckIfArrayIsInSpecificOrder(updatePages, orderingRule[0], orderingRule[1]);
-                arrayCorrectOrder.Add(isInCorrectOrder);
-            }
-
-            if (arrayCorrectOrder.All(b => b))
-            {
-                solution1 += updatePages[(updatePages.Length-1) / 2];
-            }
-            else
-            {
-                arrayToSort
-                    .Sort((left, right) => orderingRules
-                        .Any(rule => rule[0] == left && rule[1] == right) ? 1 : -1);
-                
-                solution2 += arrayToSort[(arrayToSort.Count - 1) / 2];
-            }
-        }
+        var solution1 = updatesPages
+            .Where(updatePages => IsOrdered(updatePages, orderingRules))
+            .Select(updatePages => updatePages[(updatePages.Count - 1) / 2])
+            .Sum();
         
-        Console.WriteLine($"Solution 01: {solution1}");
-        Console.WriteLine($"Solution 02: {solution2}");
+        var solution2 = updatesPages
+            .Where(updatePages => IsOrdered(updatePages, orderingRules) == false)
+            .Select(updatePages =>
+            {
+                updatePages.Sort((left, right) => orderingRules
+                    .Any(rule => rule[0] == left && rule[1] == right) ? 1 : -1);
+                return updatePages[(updatePages.Count - 1) / 2];
+            })
+            .Sum();
+        
+        Console.WriteLine($"Solution 1: {solution1}");
+        Console.WriteLine($"Solution 2: {solution2}");
     }
 }
