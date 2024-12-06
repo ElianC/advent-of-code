7705186b4993a73b2429c604bd5e1f662e5e9e89
Elian Caby <54775334+ElianC@users.noreply.github.com>
2024-12-06 21:16:08 +0100

    Update Solution.cs

--- 2024/Day06/Solution.cs
+++ 2024/Day06/Solution.cs
@@ -58,8 +58,8 @@ public class Solution : BaseSolution
         return (nextX, nextY, input[nextY][nextX]);
     }
 
-    private void GuardRound(Directions startDirection, List<List<string>> input, int startX, int startY,
-        bool keepTrackVisitedCells = false, int testX = -1, int testY = -1)
+    private void DoGuardRound(Directions startDirection, List<List<string>> input, int startX, int startY,
+        bool keepTrackVisitedCells = false)
     {
         var currentDirection = startDirection;
         var guardX = startX;
@@ -67,33 +67,20 @@ public class Solution : BaseSolution
 
         HashSet<(Directions dir, int x, int y)> isAlreadyBeenHere = new();
 
-        //Console.WriteLine($"Start {(startX, startY)}");
-        if (!keepTrackVisitedCells) Console.WriteLine($"Obstacle {(testX, testY)}");
-
-        var i = 0;
-
         do
         {
-            if (!keepTrackVisitedCells) Console.WriteLine($"Player pos {(guardX, guardY)}");
-
             var nextCell = GetNextCellInfos(currentDirection, input, guardX, guardY);
 
             if (nextCell is not null)
                 if (!isAlreadyBeenHere.Add((currentDirection, guardX, guardY)))
-                    throw new Exception("Guard has already patrolled there.");
+                    throw new Exception(
+                        $"Guard has already patrolled here, he must going around in circles. {guardX} {guardY}");
             while (nextCell is not null && nextCell.Value.value == "#")
             {
                 currentDirection = GetNextDirection(currentDirection);
-                Console.WriteLine($"changing direction {currentDirection} {testX} {testY}");
                 nextCell = GetNextCellInfos(currentDirection, input, guardX, guardY);
             }
 
-
-            /*  else if (nextCell is not null && nextCell.Value.value == "#")
-              {
-                  if (!isAlreadyBeenHere.Add((currentDirection, guardX, guardY)))
-                      break;
-              }*/
             if (nextCell is null) break;
 
             guardX = nextCell.Value.x;
@@ -124,31 +111,24 @@ public class Solution : BaseSolution
 
         _visitedCells.Add((guardX, guardY));
 
-        GuardRound(currentDirection, input, guardX, guardY, true);
-
-        List<(int x, int y)> test = [(4, 7)];
+        DoGuardRound(currentDirection, input, guardX, guardY, true);
 
         foreach (var visited in _visitedCells)
         {
-            var _input = new List<List<string>>(input.Select(e => e.ToList()));
+            var localInput = new List<List<string>>(input.Select(e => e.ToList()));
 
             if (visited.x == guardX && visited.y == guardY) continue;
-            _input[visited.y][visited.x] = "#";
+            localInput[visited.y][visited.x] = "#";
 
             try
             {
-                GuardRound(currentDirection, _input, guardX, guardY, false, visited.x, visited.y);
+                DoGuardRound(currentDirection, localInput, guardX, guardY);
             }
-            catch (Exception e)
+            catch (Exception)
             {
-                Console.WriteLine((currentDirection, visited.x, visited.y));
-
-                Console.WriteLine(e.Message);
                 solution2++;
             }
         }
-
-        foreach (var row in input) Console.WriteLine(string.Join(" ", row));
 
         solution1 += _visitedCells.Count;
 
