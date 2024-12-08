7f99e37b29a9d5ac6c7a58639186f84c9bfd312c
Elian Caby <54775334+ElianC@users.noreply.github.com>
2024-12-08 17:18:03 +0100

    clean a bit day08 part 1

--- 2024/Day08/Solution.cs
+++ 2024/Day08/Solution.cs
@@ -1,5 +1,3 @@
-using System.Text;
-
 namespace AdventOfCode._2024.Day08;
 
 public class Solution : BaseSolution
@@ -12,28 +10,23 @@ public class Solution : BaseSolution
     {
         var input = GetInput()
             .Split(Environment.NewLine)
-            //.Select(row => row.Select(e => e).ToArray())
             .ToArray();
-
-        var response = 0;
 
         var filteredInput = input
             .SelectMany((line, y) => line
-                .Select((e, x) => (e, (x, y)))
-                .Where(e => e.e != '#' && e.e != '.')
+                .Select((e, x) => (cell: e, (x, y)))
+                .Where(e => e.cell != '.')
                 .ToArray())
-            .GroupBy(e => e.e)
+            .GroupBy(e => e.cell)
             .Select(e => (e.Key, e.Select(f => f.Item2).ToArray()))
             .ToArray();
 
-        List<(int x, int y)> points = [];
+        HashSet<(int x, int y)> antiNodesPoints = [];
 
         foreach (var group in filteredInput)
         {
             var test = group.Item2.ToList();
             var currentIndex = 0;
-
-            Console.WriteLine(group.Key + "-" + string.Join(',', group.Item2));
 
             while (currentIndex < group.Item2.Length - 1)
             {
@@ -41,62 +34,25 @@ public class Solution : BaseSolution
 
                 foreach (var coords in test.Skip(currentIndex + 1))
                 {
-                    var diffX = coords.x - currentItem.x;
-                    var diffY = coords.y - currentItem.y;
+                    var diffs = (x: coords.x - currentItem.x, y: coords.y - currentItem.y);
 
-                    if (coords.y + diffY >= 0 && coords.y + diffY < input.Length &&
-                        coords.x + diffX >= 0 && coords.x + diffX < input[0].Length)
-                    {
-                        //response++;
-                        if (input[coords.y + diffY][coords.x + diffX] == '.')
-                        {
-                            response++;
-                            points.Add((coords.x + diffX, coords.y + diffY));
-                        }
+                    if (ArePointsInsideBounds(coords.x + diffs.x, coords.y + diffs.y))
+                        antiNodesPoints.Add((coords.x + diffs.x, coords.y + diffs.y));
 
-                        ;
-                        Console.WriteLine(
-                            $"{currentItem} {coords} {(coords.x + diffX, coords.y + diffY)} {diffX},{diffY}, {input[coords.y + diffY][coords.x + diffX]}");
-                    }
-                    else
-                    {
-                        Console.WriteLine($"out of bounds 1 {(coords.y + diffY, coords.x + diffX)}");
-                    }
-
-                    if (currentItem.y - diffY >= 0 && currentItem.y - diffY < input.Length &&
-                        currentItem.x - diffX >= 0 && currentItem.x - diffX < input[0].Length)
-                    {
-                        if (input[currentItem.y - diffY][currentItem.x - diffX] == '.')
-                        {
-                            response++;
-                            points.Add((currentItem.x - diffX, currentItem.y - diffY));
-                        }
-
-                        Console.WriteLine(
-                            $"2 {currentItem} {coords} {(currentItem.x - diffX, currentItem.y - diffY)} {diffX},{diffY}, {input[currentItem.y - diffY][currentItem.x - diffX]}");
-                    }
-                    else
-                    {
-                        Console.WriteLine($"out of bounds 2 {(currentItem.y - diffY, currentItem.x - diffX)}");
-                    }
-
-                    Console.WriteLine("------");
+                    if (ArePointsInsideBounds(currentItem.x - diffs.x, currentItem.y - diffs.y))
+                        antiNodesPoints.Add((currentItem.x - diffs.x, currentItem.y - diffs.y));
                 }
 
                 currentIndex++;
             }
         }
 
-        var tests = input.ToList();
-        foreach (var point in points)
-        {
-            var sb = new StringBuilder(tests[point.y]);
-            sb[point.x] = '#';
-            tests[point.y] = sb.ToString();
-        }
+        Console.WriteLine($"Solution 1 {antiNodesPoints.Count}");
+        return;
 
-        foreach (var tst in tests) Console.WriteLine(tst);
-        Console.WriteLine($"Total response: {points.Count}");
-        Console.WriteLine(response);
+        bool ArePointsInsideBounds(int x, int y)
+        {
+            return x >= 0 && x < input[0].Length && y >= 0 && y < input.Length;
+        }
     }
 }
