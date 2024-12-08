a9262e44a0fca7882091ac8fdd6ba58746c34246
Elian Caby <54775334+ElianC@users.noreply.github.com>
2024-12-08 16:48:48 +0100

    add day 7 solution 2

--- 2024/Day07/Solution.cs
+++ 2024/Day07/Solution.cs
@@ -17,6 +17,21 @@ public class Solution : BaseSolution
         return CheckOperatorsRecursively(nextTryMul, target) || CheckOperatorsRecursively(nextTryAdd, target);
     }
 
+    private static bool CheckOperatorsRecursively2(List<long> numbers, long target)
+    {
+        if (numbers.Count == 1) return numbers.First() == target;
+        var nextTry = numbers.Skip(2).ToList();
+
+        var concatNumber = long.Parse($"{numbers[0]}{numbers[1]}");
+        var nextTryMul = nextTry.Prepend(numbers[0] * numbers[1]).ToList();
+        var nextTryAdd = nextTry.Prepend(numbers[0] + numbers[1]).ToList();
+        var nextTryConcat = nextTry.Prepend(concatNumber).ToList();
+
+        return CheckOperatorsRecursively2(nextTryMul, target) || CheckOperatorsRecursively2(nextTryAdd, target) ||
+               CheckOperatorsRecursively2(nextTryConcat, target);
+    }
+
+
     public override void Solve()
     {
         var input = GetInput()
@@ -31,6 +46,11 @@ public class Solution : BaseSolution
             .Where(e => CheckOperatorsRecursively(e.ops, e.res))
             .Sum(e => e.res);
 
+        var Solution2 = input
+            .Where(e => CheckOperatorsRecursively2(e.ops, e.res))
+            .Sum(e => e.res);
+
         Console.WriteLine($"{solution1}");
+        Console.WriteLine($"{Solution2}");
     }
 }
