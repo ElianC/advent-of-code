e5ad84360479e63f4aa0988402d34e6a1a28e400
Elian Caby <54775334+ElianC@users.noreply.github.com>
2024-12-08 18:05:31 +0100

    feat: refactor

--- 2024/Day08/Solution.cs
+++ 2024/Day08/Solution.cs
@@ -18,41 +18,61 @@ public class Solution : BaseSolution
                 .Where(e => e.cell != '.')
                 .ToArray())
             .GroupBy(e => e.cell)
-            .Select(e => (e.Key, e.Select(f => f.Item2).ToArray()))
+            .Select(e => (cell: e.Key, e.Select(f => f.Item2).ToArray()))
             .ToArray();
 
         HashSet<(int x, int y)> antiNodesPoints = [];
+        HashSet<(int x, int y)> antiNodesPoints2 = [];
 
         foreach (var group in filteredInput)
         {
-            var test = group.Item2.ToList();
-            var currentIndex = 0;
+            var points = group.Item2.ToList();
 
-            while (currentIndex < group.Item2.Length - 1)
+            for (var i = 0; i < group.Item2.Length - 1; i++)
             {
-                var currentItem = group.Item2[currentIndex];
+                var currentItem = group.Item2[i];
 
-                foreach (var coords in test.Skip(currentIndex + 1))
+                foreach (var coords in points.Skip(i + 1))
                 {
                     var diffs = (x: coords.x - currentItem.x, y: coords.y - currentItem.y);
 
-                    if (ArePointsInsideBounds(coords.x + diffs.x, coords.y + diffs.y))
-                        antiNodesPoints.Add((coords.x + diffs.x, coords.y + diffs.y));
+                    if (ArePointsInsideBounds(currentItem) &&
+                        ArePointsInsideBounds(coords))
+                    {
+                        antiNodesPoints2.Add(currentItem);
+                        antiNodesPoints2.Add(coords);
+                    }
 
-                    if (ArePointsInsideBounds(currentItem.x - diffs.x, currentItem.y - diffs.y))
-                        antiNodesPoints.Add((currentItem.x - diffs.x, currentItem.y - diffs.y));
+                    var descTest = (x: coords.x + diffs.x, y: coords.y + diffs.y);
+                    var ascTest = (x: currentItem.x - diffs.x, y: currentItem.y - diffs.y);
+
+                    if (ArePointsInsideBounds(descTest)) antiNodesPoints.Add(descTest);
+
+                    while (ArePointsInsideBounds(descTest))
+                    {
+                        antiNodesPoints2.Add(descTest);
+                        descTest.x += diffs.x;
+                        descTest.y += diffs.y;
+                    }
+
+                    if (ArePointsInsideBounds(ascTest)) antiNodesPoints.Add(ascTest);
+
+                    while (ArePointsInsideBounds(ascTest))
+                    {
+                        antiNodesPoints2.Add(ascTest);
+                        ascTest.x -= diffs.x;
+                        ascTest.y -= diffs.y;
+                    }
                 }
-
-                currentIndex++;
             }
         }
 
         Console.WriteLine($"Solution 1 {antiNodesPoints.Count}");
-        return;
+        Console.WriteLine($"Solution 2 {antiNodesPoints2.Count}");
 
-        bool ArePointsInsideBounds(int x, int y)
+        bool ArePointsInsideBounds((int x, int y) pos)
         {
-            return x >= 0 && x < input[0].Length && y >= 0 && y < input.Length;
+            return pos.x >= 0 && pos.x < input[0].Length && pos.y >= 0 && pos.y < input.Length;
         }
     }
 }
