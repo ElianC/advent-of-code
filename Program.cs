b2edfdfea7e70e6c2384878e5e4796b36ca5133b
Elian Caby <54775334+ElianC@users.noreply.github.com>
2024-12-09 10:52:54 +0100

    abstract invoke of solution solve method to dedicated method

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
-﻿using System.Reflection;
-using AdventOfCode;
+﻿using AdventOfCode;
 using Microsoft.Extensions.Configuration;
 
 IConfiguration config = new ConfigurationBuilder()
@@ -16,34 +15,4 @@ var targetYear = argYear ?? currentYear;
 var targetDay = (argDay > 0 ? argDay : currentDay).ToString("00");
 
 SolutionLoader.CreateInstance(targetDay, targetYear);
-var instanceSolution = SolutionLoader.GetInstance();
-var assembly = Assembly.GetExecutingAssembly();
-var className = $"AdventOfCode._{targetYear}.Day{targetDay}.Solution";
-
-var type = assembly.GetType(className);
-
-if (type is null)
-{
-    Console.WriteLine($"The class for the day {targetDay} of year {targetYear} was not found. '{className}'");
-    return;
-}
-
-var instance = Activator.CreateInstance(type);
-if (instance is null)
-{
-    Console.WriteLine($"Unable to create an instance of '{className}'.");
-    return;
-}
-
-// Find a method to invoke (e.g., a Run or Execute method)
-var method = type.GetMethod("Solve");
-
-if (method == null)
-{
-    Console.WriteLine(
-        $"Method 'Solve' not found in class '{className}'. Ensure that the class extend the class BaseSolution");
-    return;
-}
-
-// Invoke the method
-method.Invoke(instance, null);
+SolutionLoader.InvokeSolutionSolve();
--- SolutionLoader.cs
+++ SolutionLoader.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics;
+using System.Reflection;
 
 namespace AdventOfCode;
 
@@ -24,5 +25,42 @@ public sealed class SolutionLoader
     {
         Debug.Assert(_instance != null, nameof(_instance) + " != null");
         return _instance;
+    }
+
+    public static void InvokeSolutionSolve()
+    {
+        var instance = GetInstance();
+
+        var assembly = Assembly.GetExecutingAssembly();
+        var className = $"AdventOfCode._{instance.Year}.Day{instance.Day}.Solution";
+
+        var type = assembly.GetType(className);
+
+        if (type is null)
+        {
+            Console.WriteLine(
+                $"The class for the day {instance.Day} of year {instance.Year} was not found. '{className}'");
+            return;
+        }
+
+        var activatorInstance = Activator.CreateInstance(type);
+        if (activatorInstance is null)
+        {
+            Console.WriteLine($"Unable to create an instance of '{className}'.");
+            return;
+        }
+
+        // Find a method to invoke (e.g., a Run or Execute method)
+        var method = type.GetMethod("Solve");
+
+        if (method is null)
+        {
+            Console.WriteLine(
+                $"Method 'Solve' not found in class '{className}'. Ensure that the class extend the class BaseSolution");
+            return;
+        }
+
+        // Invoke the method
+        method.Invoke(activatorInstance, null);
     }
 }
