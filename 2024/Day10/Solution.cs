c20e5acf70b1561819f427399042f378caf9eb4b
Elian Caby <54775334+ElianC@users.noreply.github.com>
2024-12-09 19:50:31 +0100

    prepare day 10

---
+++ 2024/Day10/Solution.cs
@@ -0,0 +1,10 @@
+namespace AdventOfCode._2024.Day10;
+
+public class Solution : BaseSolution
+{
+    public override void Solve()
+    {
+        var input = GetInput();
+        Console.WriteLine("Day 10");
+    }
+}
