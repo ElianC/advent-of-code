5af5cd9d411c9d19cc4f77b6e1f164c66174e547
Elian Caby <54775334+ElianC@users.noreply.github.com>
2024-12-13 23:14:15 +0100

    Update Solution.cs

--- 2024/Day08/Solution.cs
+++ 2024/Day08/Solution.cs
@@ -1,24 +1,30 @@
+using System.Drawing;
+using AdventOfCode.Utils;
+
 namespace AdventOfCode._2024.Day08;
 
 public class Solution : BaseSolution
 {
+    private readonly Grid<char> _grid;
+
+    public Solution()
+    {
+        _grid = new Grid<char>(GetInput());
+    }
+
     public override void Solve()
     {
-        var input = GetInput()
-            .Split(Environment.NewLine)
-            .ToArray();
-
-        var antennasGroupedByFrequencies = input
-            .SelectMany((line, y) => line
-                .Select((e, x) => new { frequency = e, pos = (x, y) })
+        var antennasGroupedByFrequencies = _grid.GetGrid()
+            .SelectMany(line => line
+                .Select(e => new { frequency = e.Value, pos = e.Point })
                 .Where(e => e.frequency != '.')
                 .ToArray())
             .GroupBy(e => e.frequency)
             .Select(e => new { frequency = e.Key, pos = e.Select(f => f.pos).ToArray() })
             .ToArray();
 
-        HashSet<(int x, int y)> antiNodesPos = [];
-        HashSet<(int x, int y)> antiNodesPosAndAntennas = [];
+        HashSet<Point> antiNodesPos = [];
+        HashSet<Point> antiNodesPosAndAntennas = [];
 
         foreach (var antennasFrequencyGroup in antennasGroupedByFrequencies)
             for (var i = 0; i < antennasFrequencyGroup.pos.Length - 1; i++)
@@ -28,38 +34,31 @@ public class Solution : BaseSolution
 
                 foreach (var comparePos in antennasFrequencyGroup.pos.Skip(i + 1))
                 {
-                    var posDiff = (x: comparePos.x - pos.x, y: comparePos.y - pos.y);
+                    var posDiff = new Size(comparePos) - new Size(pos);
                     antiNodesPosAndAntennas.Add(comparePos);
 
-                    var posNextAntiNode = (x: comparePos.x + posDiff.x, y: comparePos.y + posDiff.y);
-                    var posPrevAntiNode = (x: pos.x - posDiff.x, y: pos.y - posDiff.y);
+                    var posNextAntiNode = comparePos + posDiff;
+                    var posPrevAntiNode = pos - posDiff;
 
-                    if (ArePosInsideBounds(posNextAntiNode)) antiNodesPos.Add(posNextAntiNode);
+                    if (_grid.ContainsPoint(posNextAntiNode)) antiNodesPos.Add(posNextAntiNode);
 
-                    while (ArePosInsideBounds(posNextAntiNode))
+                    while (_grid.ContainsPoint(posNextAntiNode))
                     {
                         antiNodesPosAndAntennas.Add(posNextAntiNode);
-                        posNextAntiNode.x += posDiff.x;
-                        posNextAntiNode.y += posDiff.y;
+                        posNextAntiNode += posDiff;
                     }
 
-                    if (ArePosInsideBounds(posPrevAntiNode)) antiNodesPos.Add(posPrevAntiNode);
+                    if (_grid.ContainsPoint(posPrevAntiNode)) antiNodesPos.Add(posPrevAntiNode);
 
-                    while (ArePosInsideBounds(posPrevAntiNode))
+                    while (_grid.ContainsPoint(posPrevAntiNode))
                     {
                         antiNodesPosAndAntennas.Add(posPrevAntiNode);
-                        posPrevAntiNode.x -= posDiff.x;
-                        posPrevAntiNode.y -= posDiff.y;
+                        posPrevAntiNode -= posDiff;
                     }
                 }
             }
 
         Console.WriteLine($"Solution 1 {antiNodesPos.Count}");
         Console.WriteLine($"Solution 2 {antiNodesPosAndAntennas.Count}");
-
-        bool ArePosInsideBounds((int x, int y) pos)
-        {
-            return pos.x >= 0 && pos.x < input[0].Length && pos.y >= 0 && pos.y < input.Length;
-        }
     }
 }
