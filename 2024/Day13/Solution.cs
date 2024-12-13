f237059097c3f34b9173e17417591d884607ac78
Elian Caby <caby.elian01@gmail.com>
2024-12-13 10:05:05 +0100

    try optimization 1

--- 2024/Day13/Solution.cs
+++ 2024/Day13/Solution.cs
@@ -5,10 +5,11 @@ namespace AdventOfCode._2024.Day13;
 
 public class Solution : BaseSolution
 {
-    private HashSet<int> AutoSpin(Point winningPoint, Size offsetA, Size offsetB)
+    private long? AutoSpin((long X, long Y) winningPoint, Size offsetA, Size offsetB)
     {
-        HashSet<int> creditsSpent = [];
-        List<int> maxSpinsPossible = [
+        long? smallestCreditsAmount = null;
+        
+        List<long> maxSpinsPossible = [
             winningPoint.X / offsetA.Width, 
             winningPoint.Y / offsetA.Height,
             winningPoint.X / offsetB.Width, 
@@ -17,17 +18,21 @@ public class Solution : BaseSolution
 
         var maxSpinPossible = maxSpinsPossible.Max();
             
+        Console.WriteLine($"Max spins possible: {maxSpinPossible}");
         for (var i = 1; i < maxSpinPossible; i++)
         {
             List<(Size offset, int price)> spins = [];
 
-            Enumerable.Range(0, i)
-                .ToList()
-                .ForEach(_ => spins.Add((offsetA, 3)));   
-            Enumerable.Range(i, maxSpinPossible - i)
-                .ToList()
-                .ForEach(_ => spins.Add((offsetB, 1)));
-                
+            for (long a = 0; a < i; a++)
+            {
+                spins.Add((offsetA, 3));
+            }           
+            
+            for (long a = i; a < maxSpinPossible - i; a++)
+            {
+                spins.Add((offsetB, 1));
+            }
+            
             var creditsSpentForTry = 0;
             var currentPoint = new Point(0, 0);
                 
@@ -36,17 +41,17 @@ public class Solution : BaseSolution
                 creditsSpentForTry += spin.price;
                 currentPoint += spin.offset;
                     
-                var isWiningSpin = winningPoint == currentPoint;
+                var isWiningSpin = winningPoint.X == currentPoint.X && winningPoint.Y == currentPoint.Y;
 
-                if (isWiningSpin)
+                if (isWiningSpin && (smallestCreditsAmount is null || creditsSpentForTry < smallestCreditsAmount))
                 {
-                    creditsSpent.Add(creditsSpentForTry);
+                    smallestCreditsAmount = creditsSpentForTry; 
                     break;
                 }
             }
         }
         
-        return creditsSpent;
+        return smallestCreditsAmount;
     }
     
     public override void Solve()
@@ -56,11 +61,10 @@ public class Solution : BaseSolution
             .Select(e => e.Split(Environment.NewLine)).ToArray();
         
         long totalCreditsSpent = 0;
+        long totalCreditsSpentAdjusted = 0;
         
         foreach (var machineInfos in slotMachines)
         {
-            //List<int> creditsSpent = new List<int>();
-            
             var regex = new Regex(@"\d+");
             var buttonA = regex.Matches(machineInfos[0])
                 .Select(e => e.ToString())
@@ -77,15 +81,26 @@ public class Solution : BaseSolution
             
             var offsetA = new Size(buttonA[0], buttonA[1]);
             var offsetB = new Size(buttonB[0], buttonB[1]);
-            var prizePoint = new Point(prize[0], prize[1]);
-            var creditsSpent = AutoSpin(prizePoint, offsetA, offsetB);
+            var prizePoint = (prize[0], prize[1]);
+
+            var offsetWinningPoint = 10000000000000;
+            var prizePointAdjusted = (prize[0] + offsetWinningPoint, prize[1] + offsetWinningPoint);
             
-           if (creditsSpent.Any())
+            var creditsSpent = AutoSpin(prizePoint, offsetA, offsetB);
+            var creditsSpentAdjusted = AutoSpin(prizePointAdjusted, offsetA, offsetB);
+            
+           if (creditsSpent is not null)
             {
-                totalCreditsSpent += creditsSpent.Min();
+                totalCreditsSpent += (long)creditsSpent;
+            }
+
+            if (creditsSpentAdjusted is not null)
+            {
+                totalCreditsSpentAdjusted += (long)creditsSpentAdjusted;
             }
         }
         
         Console.WriteLine(totalCreditsSpent);
+        Console.WriteLine(totalCreditsSpentAdjusted);
     }
 }
