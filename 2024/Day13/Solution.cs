afe7987351b5b7a7b7cea926b04dac3a0dda9a20
Elian Caby <caby.elian01@gmail.com>
2024-12-13 09:47:47 +0100

    Abstract AutoSpin to a function

--- 2024/Day13/Solution.cs
+++ 2024/Day13/Solution.cs
@@ -5,10 +5,48 @@ namespace AdventOfCode._2024.Day13;
 
 public class Solution : BaseSolution
 {
-    private bool AutoSpin(Point winningPoint, Point point)
+    private HashSet<int> AutoSpin(Point winningPoint, Size offsetA, Size offsetB)
     {
-        if (point == winningPoint) return true;
-        return false;
+        HashSet<int> creditsSpent = [];
+        List<int> maxSpinsPossible = [
+            winningPoint.X / offsetA.Width, 
+            winningPoint.Y / offsetA.Height,
+            winningPoint.X / offsetB.Width, 
+            winningPoint.Y / offsetB.Height
+        ];
+
+        var maxSpinPossible = maxSpinsPossible.Max();
+            
+        for (var i = 1; i < maxSpinPossible; i++)
+        {
+            List<(Size offset, int price)> spins = [];
+
+            Enumerable.Range(0, i)
+                .ToList()
+                .ForEach(_ => spins.Add((offsetA, 3)));   
+            Enumerable.Range(i, maxSpinPossible - i)
+                .ToList()
+                .ForEach(_ => spins.Add((offsetB, 1)));
+                
+            var creditsSpentForTry = 0;
+            var currentPoint = new Point(0, 0);
+                
+            foreach (var spin in spins)
+            {
+                creditsSpentForTry += spin.price;
+                currentPoint += spin.offset;
+                    
+                var isWiningSpin = winningPoint == currentPoint;
+
+                if (isWiningSpin)
+                {
+                    creditsSpent.Add(creditsSpentForTry);
+                    break;
+                }
+            }
+        }
+        
+        return creditsSpent;
     }
     
     public override void Solve()
@@ -21,7 +59,7 @@ public class Solution : BaseSolution
         
         foreach (var machineInfos in slotMachines)
         {
-            List<int> creditsSpent = new List<int>();
+            //List<int> creditsSpent = new List<int>();
             
             var regex = new Regex(@"\d+");
             var buttonA = regex.Matches(machineInfos[0])
@@ -32,54 +70,17 @@ public class Solution : BaseSolution
                 .Select(e => e.ToString())
                 .Select(int.Parse)
                 .ToArray();            
-            var Prize = regex.Matches(machineInfos[2])
+            var prize = regex.Matches(machineInfos[2])
                 .Select(e => e.ToString())
                 .Select(int.Parse)
                 .ToArray();
             
             var offsetA = new Size(buttonA[0], buttonA[1]);
             var offsetB = new Size(buttonB[0], buttonB[1]);
-            var prize = new Point(Prize[0], Prize[1]);
-
-            List<int> maxSpinsPossible = [
-                prize.X / offsetA.Width, 
-                prize.Y / offsetA.Height,
-                prize.X / offsetB.Width, 
-                prize.Y / offsetB.Height
-            ];
-
-            var maxSpinPossible = maxSpinsPossible.Max();
+            var prizePoint = new Point(prize[0], prize[1]);
+            var creditsSpent = AutoSpin(prizePoint, offsetA, offsetB);
             
-            for (var i = 1; i < maxSpinPossible; i++)
-            {
-                List<(Size offset, int price)> spins = [];
-
-                Enumerable.Range(0, i)
-                    .ToList()
-                    .ForEach(_ => spins.Add((offsetA, 3)));   
-                Enumerable.Range(i, maxSpinPossible - i)
-                    .ToList()
-                    .ForEach(_ => spins.Add((offsetB, 1)));
-                
-                var creditsSpentForTry = 0;
-                var currentPoint = new Point(0, 0);
-                
-                foreach (var spin in spins)
-                {
-                    creditsSpentForTry += spin.price;
-                    currentPoint += spin.offset;
-                    
-                    var isWiningSpin = AutoSpin(prize, currentPoint);
-
-                    if (isWiningSpin)
-                    {
-                        creditsSpent.Add(creditsSpentForTry);
-                        break;
-                    }
-                }
-            }
-
-            if (creditsSpent.Any())
+           if (creditsSpent.Any())
             {
                 totalCreditsSpent += creditsSpent.Min();
             }
