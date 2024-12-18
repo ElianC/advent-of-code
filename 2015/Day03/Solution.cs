77360d45f1596f21b62ab4e1451f14943b7f7510
Elian Caby <54775334+ElianC@users.noreply.github.com>
2024-12-18 20:59:28 +0100

    resolving

--- 2015/Day03/Solution.cs
+++ 2015/Day03/Solution.cs
@@ -4,7 +4,10 @@ namespace AdventOfCode._2015.Day03;
 
 public class Solution : BaseSolution
 {
-    private readonly List<( char, Size)> _cellGuardMatchDirection =
+    private readonly HashSet<Point> _housesVisitedBySanta = [];
+    private readonly HashSet<Point> _housesVisitedBySantaAndRobot = [];
+
+    private readonly List<(char instruction, Size direction)> _instructionsDirectionsPair =
     [
         ('^', new Size(0, -1)),
         ('>', new Size(1, 0)),
@@ -12,21 +15,43 @@ public class Solution : BaseSolution
         ('<', new Size(-1, 0))
     ];
 
-    private readonly HashSet<Point> points = new();
-
     public override void Solve()
     {
-        var input = GetInput();
-        var pos = Point.Empty;
+        var instructions = GetInput();
 
-        foreach (var dir in input)
+        var positionSantaAlone = Point.Empty;
+        var positionSanta = Point.Empty;
+        var positionRobot = Point.Empty;
+
+        _housesVisitedBySantaAndRobot.Add(positionSanta);
+
+        for (var i = 0; i < instructions.Length; i++)
         {
-            points.Add(pos);
-            var matching = _cellGuardMatchDirection.Where(e => e.Item1 == dir).Select(e => e.Item2).Single();
+            var instruction = instructions[i];
+            var direction = _instructionsDirectionsPair
+                .Where(e => e.instruction == instruction)
+                .Select(e => e.direction)
+                .Single();
 
-            pos += matching;
+            positionSantaAlone += direction;
+            _housesVisitedBySanta.Add(positionSantaAlone);
+
+            if (i % 2 == 0)
+            {
+                positionSanta += direction;
+                _housesVisitedBySantaAndRobot.Add(positionSanta);
+            }
+            else
+            {
+                positionRobot += direction;
+                _housesVisitedBySantaAndRobot.Add(positionRobot);
+            }
         }
 
-        Console.WriteLine($"{points.Count}");
+        Console.WriteLine("{0} houses have received a gift by santa.",
+            _housesVisitedBySanta.Count
+        );
+        Console.WriteLine("{0} houses have received a gift by santa and santa robot.",
+            _housesVisitedBySantaAndRobot.Count);
     }
 }
