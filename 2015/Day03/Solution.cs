1301c752d6c0ce2ef3d2f3e8f42603c437306b4a
Elian Caby <54775334+ElianC@users.noreply.github.com>
2024-12-18 21:06:45 +0100

    Update Solution.cs

--- 2015/Day03/Solution.cs
+++ 2015/Day03/Solution.cs
@@ -4,47 +4,46 @@ namespace AdventOfCode._2015.Day03;
 
 public class Solution : BaseSolution
 {
+    private readonly Dictionary<char, Size> _directions = new()
+    {
+        { '^', new Size(0, -1) },
+        { '>', new Size(1, 0) },
+        { 'v', new Size(0, 1) },
+        { '<', new Size(-1, 0) }
+    };
+
     private readonly HashSet<Point> _housesVisitedBySanta = [];
     private readonly HashSet<Point> _housesVisitedBySantaAndRobot = [];
 
-    private readonly List<(char instruction, Size direction)> _instructionsDirectionsPair =
-    [
-        ('^', new Size(0, -1)),
-        ('>', new Size(1, 0)),
-        ('v', new Size(0, 1)),
-        ('<', new Size(-1, 0))
-    ];
 
     public override void Solve()
     {
         var instructions = GetInput();
 
-        var positionSantaAlone = Point.Empty;
-        var positionSanta = Point.Empty;
-        var positionRobot = Point.Empty;
+        var santaAlonePosition = Point.Empty;
+        var santaPosition = Point.Empty;
+        var robotPosition = Point.Empty;
 
-        _housesVisitedBySantaAndRobot.Add(positionSanta);
+        _housesVisitedBySantaAndRobot.Add(santaPosition);
+        _housesVisitedBySanta.Add(santaAlonePosition);
 
         for (var i = 0; i < instructions.Length; i++)
         {
             var instruction = instructions[i];
-            var direction = _instructionsDirectionsPair
-                .Where(e => e.instruction == instruction)
-                .Select(e => e.direction)
-                .Single();
+            var direction = _directions[instruction];
 
-            positionSantaAlone += direction;
-            _housesVisitedBySanta.Add(positionSantaAlone);
+            santaAlonePosition += direction;
+            _housesVisitedBySanta.Add(santaAlonePosition);
 
             if (i % 2 == 0)
             {
-                positionSanta += direction;
-                _housesVisitedBySantaAndRobot.Add(positionSanta);
+                santaPosition += direction;
+                _housesVisitedBySantaAndRobot.Add(santaPosition);
             }
             else
             {
-                positionRobot += direction;
-                _housesVisitedBySantaAndRobot.Add(positionRobot);
+                robotPosition += direction;
+                _housesVisitedBySantaAndRobot.Add(robotPosition);
             }
         }
 
