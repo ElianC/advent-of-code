5320f6bcca740cc113de1bd6a65df1ca97570000
Elian Caby <54775334+ElianC@users.noreply.github.com>
2024-12-24 11:06:42 +0100

    resolving part 2

--- 2015/Day04/Solution.cs
+++ 2015/Day04/Solution.cs
@@ -5,7 +5,7 @@ namespace AdventOfCode._2015.Day04;
 
 public class Solution : BaseSolution
 {
-    public override void Solve()
+    public (int result, string output) FindHashStartsWith(string startsWith)
     {
         var input = GetInput();
 
@@ -18,12 +18,23 @@ public class Solution : BaseSolution
 
             var message = input + i;
             var inputBytes = Encoding.UTF8.GetBytes(message);
-            var hashBytes = MD5.Create().ComputeHash(inputBytes);
+            var hashBytes = MD5.HashData(inputBytes);
             output = Convert.ToHexString(hashBytes);
-        } while (!output.StartsWith(new string('0', 5)));
+        } while (!output.StartsWith(startsWith));
+
+        return (i, output);
+    }
+
+    public override void Solve()
+    {
+        var solution1 = FindHashStartsWith(new string('0', 5));
+        var solution2 = FindHashStartsWith(new string('0', 6));
 
         Console.WriteLine("The least number to produce an hash that starts with 5 Zeros is {0}, and the hash is {1}",
-            i,
-            output);
+            solution1.result,
+            solution1.output);
+        Console.WriteLine("The least number to produce an hash that starts with 6 Zeros is {0}, and the hash is {1}",
+            solution2.result,
+            solution2.output);
     }
 }
